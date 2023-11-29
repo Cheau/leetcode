ce3294bf401af0c54915a39e88c41f570945a5af
Cheau <liqinb@csair.com>
2023-11-29 11:12:39 +0800

    Fixed multiple dimension array not replicated issue.

--- tests/core/test/base/Replicable.java
+++ tests/core/test/base/Replicable.java
@@ -6,16 +6,18 @@ import java.util.Arrays;
 interface Replicable {
 
     default Object[] replicate(Object[] args) {
-        return Arrays.stream(args)
-                .map(arg -> {
-                    if (arg.getClass().isArray()) {
-                        int length = Array.getLength(arg);
-                        Object replica = Array.newInstance(arg.getClass().getComponentType(), length);
-                        System.arraycopy(arg, 0, replica, 0, length);
-                        return replica;
-                    }
-                    return arg;
-                })
-                .toArray();
+        return Arrays.stream(args).map(this::replicate).toArray();
+    }
+
+    private Object replicate(Object arg) {
+        if (arg.getClass().isArray()) {
+            int length = Array.getLength(arg);
+            Object replica = Array.newInstance(arg.getClass().getComponentType(), length);
+            for (int i = 0; i < length; i++) {
+                Array.set(replica, i, replicate(Array.get(arg, i)));
+            }
+            return replica;
+        }
+        return arg;
     }
 }
